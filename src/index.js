47e9b3b064222b93a2c767c3efcc4acbb7bda3e9
Daniel <danydeitu18@gmail.com>
2022-01-07 16:00:52 -0300

    subo TP app con React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/css/master.css';
+import Web from './components/Web';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web />
   </React.StrictMode>,
   document.getElementById('root')
 );
